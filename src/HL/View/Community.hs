1ae0059c7878f6bb925231afe9ac88af0a74d43c
Richard Cook <rcook@rcook.org>
2018-03-28 09:52:32 -0700

    Update Seattle Area Haskell Users' Group link

--- src/HL/View/Community.hs
+++ src/HL/View/Community.hs
@@ -67,7 +67,7 @@ offline =
      li_ (a_ [href_ "http://haskell.jp/"] "Japan Haskell Users Group (Haskell-jp)")
      li_ (a_ [href_ "http://www.meetup.com/NY-Haskell/"] "New York Haskell Users Group")
      li_ (a_ [href_ "http://www.meetup.com/London-Haskell/"] "London Haskell")
-     li_ (a_ [href_ "http://www.meetup.com/seahug/"] "Seattle Area Haskell Users' Group")
+     li_ (a_ [href_ "http://seattlehaskell.org/"] "Seattle Area Haskell Users' Group (SeaHUG)")
      li_ (a_ [href_ "http://www.meetup.com/find/?allMeetups=true&keywords=Haskell&radius=Infinity"] "More Haskell meetups at meetup.com")
 
 academicConferences :: Html ()
